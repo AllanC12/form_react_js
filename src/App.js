e523807bf3aa7f073c4f5bf6c9cf309524c63605
AllanC12 <allandevfront@gmail.com>
2023-02-19 01:49:04 -0300

    Inserindo new user no json-server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
- import './App.css';
+import './App.css';
 
+import { BrowserRouter } from 'react-router-dom';
  
- import { BrowserRouter , Routes , Route } from 'react-router-dom';
- 
- import FormAddUser from './pages/FormAddUser'
- import FormLogin from './components/FormLogin';
+import FormLogin from './components/FormLogin';
+
+import { useFetch } from './hooks/useFetch';
+
+import { useState } from 'react';
 
 function App() {
+
+   const url = `http://localhost:3000/users`
+
+   const { data:users , insertUser } = useFetch(url)
+
+ 
+   const [nameUser , setNameUser] = useState(null)
+   const [passwordUser , setPasswordUser ] = useState(null)
+
+   const [newUser,setNewUser] = useState(null)
+   const [emailUser , setEmailUser ] = useState(null)
+   const [newPasswordUser , setNewPasswordUser ] = useState(null)
+   const [validatePassword , setValidatePassword] = useState(null)
+
+    const loginUser = (e) => {
+         e.preventDefault()
+
+         if(nameUser === null || passwordUser === null){
+            alert('insira informações de login')
+         }
+
+         
+         users && users.forEach((user)=>(
+            nameUser === user.name ? (
+               passwordUser === user.password ? (
+                  alert(`Seja bem vindo ${user.name}`)
+               ) : (
+                  alert('senha incorreta')
+               )
+            ) :(
+               alert('usuário não encontrado =(')
+            )
+         ))
+    }
+
+
+    const addNewUser = (e) => {
+         e.preventDefault()
+
+         const user = {
+            "name": newUser,
+            "email":emailUser,
+            "password":newPasswordUser
+         }
+
+         insertUser(user,"POST")
+
+    }
 
    return (
     <div className="App">
        <BrowserRouter>
-           <FormLogin/> 
+           <FormLogin setNameUser={setNameUser} 
+                      setPasswordUser={setPasswordUser} 
+                      loginUser={loginUser}
+                      addNewUser={addNewUser}
+                      setNewUser={setNewUser}
+                      setNewPasswordUser={setNewPasswordUser}
+                      setEmailUser={setEmailUser}
+                      setValidatePassword={setValidatePassword}
+            /> 
        </BrowserRouter>
     </div>
   );
--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -9,31 +9,35 @@ import { NavLink } from 'react-router-dom'
 import { FaLock , FaUser , FaEnvelope } from 'react-icons/fa'
 
 //Components
-import FormAddUser from '../pages/FormAddUser'
-  
-const FormLogin = () => {
+   
+const FormLogin = ({loginUser,setNameUser , setPasswordUser , addNewUser , 
+  setNewUser , setNewPasswordUser , setEmailUser , setValidatePassword
+} ) => {
 
   const form_login = useRef()
   const form_add_user = useRef()
   const [valueBtn,setValueBtn] = useState('Login')
 
-   
   const handleForm = (element) => {
  
      if(element.innerText === 'Add user'){
+
         form_login.current.style.setProperty('display', 'none')
         form_add_user.current.style.setProperty('display', 'block')
         setValueBtn('Create User')
+
      }else{
+
         form_login.current.style.setProperty('display','block')
         form_add_user.current.style.setProperty('display','none')
         setValueBtn('Login')
+
      }
 
   }
 
    return (
-      <form>
+      <div className="wraper-forms" >
         <div className="container-form">
           
             <div className="photo-user">
@@ -52,55 +56,61 @@ const FormLogin = () => {
 
             </div>
 
-               <div ref={form_login} className="form-login">
+            <form onSubmit={loginUser} ref={form_login} className="form-login">
 
-                  <div className="wraper">
-                      <FaUser/>
-                      <input type="text" placeholder="| Username"/>
-                    </div>
+              <div className="wraper">
+                  <FaUser/>
+                  <input  required onChange={(e) => setNameUser(e.target.value)} type="text" placeholder="| Username"/>
+                </div>
 
-                    <div className="wraper">
-                      <FaLock/>
-                      <input type="password" placeholder="| Password"/>
-                    </div>
-              </div>
+                <div className="wraper">
+                  <FaLock/>
+                  <input  required onChange={(e)=> setPasswordUser(e.target.value)} type="password" placeholder="| Password"/>
+                </div>
 
-       <div ref={form_add_user} className="form_add_user">
+                    
+                  <div className="user-data">
+                    <label>
+                      <input type="checkbox" />
+                        Remember me?
+                    </label>
+                  </div>
+
+                <input type="submit" value={valueBtn} />
+
+            </form>
+
+       <form onSubmit={addNewUser} ref={form_add_user} className="form_add_user">
+
           <div className="wraper">
               <FaUser/>
-              <input type="text" placeholder="| Full name"/>
+              <input required onChange={(e)=>setNewUser(e.target.value)} type="text" placeholder="| Username"/>
            </div>
 
             <div className="wraper">
               <FaEnvelope/>
-              <input type="text" placeholder="| Your email"/>
+              <input required onChange={(e)=>setEmailUser(e.target.value)} type="text" placeholder="| Your email"/>
             </div>
 
             <div className="wraper">
               <FaLock/>
-              <input type="password" placeholder="| Password"/>
+              <input required onChange={(e)=>setNewPasswordUser(e.target.value)} type="password" placeholder="| Password"/>
             </div>
 
             <div className="wraper">
               <FaLock/>
-              <input type="password" placeholder="| Confirm the password"/>
-            </div>
-            
-       </div>
-
-             <div className="user-data">
-                <label>
-                  <input type="checkbox" />
-                    Remember me?
-                </label>
+              <input required onChange={(e)=> setValidatePassword(e.target.value)} type="password" placeholder="| Confirm the password"/>
             </div>
             
             <input type="submit" value={valueBtn} />
+       </form>
+
+            
 
           </div>
    
 
-     </form>
+     </div>
   )
 }
 
